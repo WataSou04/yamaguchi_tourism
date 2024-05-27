c26cbd0e72946405459c0b5fdc44ed35b45aa14a
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-05-27 18:35:28 +0000

    [Fix]削除後

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,8 +1,8 @@
 class Admin::CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
-    @review = @comment.review.id
+    @review = @comment.review
     @comment.destroy
-    redirect_to admin_tourist_spot_review_path(@review.tourist_spot.id, @review.id)
+    redirect_to admin_tourist_spot_review_path(@review.tourist_spot_id, @review.id)
   end
 end
